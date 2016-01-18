7cff48490101e2006165469fbc58868261cee316
Viewtiful <viewtiful@mail.ru>
2016-01-18 16:00:54 +0300

    Убрал int-ы так как они не нужны

--- Amharic/amh.py
+++ Amharic/amh.py
@@ -3,9 +3,9 @@ lines = [line.split() for line in table]#что-то вроде матрицы m
 dct = {}
 for i in range(1,len(lines)):
         for j in range(1, len(lines[i])):
-            vowel = lines[0][int(j)]
-            consonant = lines[int(i)][0]
-            dct[lines[int(i)][int(j)]] = consonant+vowel
+            vowel = lines[0][j]
+            consonant = lines[i][0]
+            dct[lines[i][j]] = consonant+vowel
 table.close()
 with open('amh.txt', encoding ='utf-8') as infile, open('result.txt', 'w', encoding ='utf-8') as outfile:
     for line in infile:
